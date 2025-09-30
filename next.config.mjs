a5b83fd31d085c9609b673ec08893cd21564826a
ronilson santos <ronilson.stos@gmail.com>
2025-09-30 20:10:39 -0300

    create page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Ativa verificações extras do React em dev
+  reactStrictMode: true,
+
+  // ⚡ Força o Next a usar PostCSS no lugar do LightningCSS (que não funciona no ARM/Termux)
+  experimental: {
+    optimizeCss: false,
+  },
+};
 
 export default nextConfig;
